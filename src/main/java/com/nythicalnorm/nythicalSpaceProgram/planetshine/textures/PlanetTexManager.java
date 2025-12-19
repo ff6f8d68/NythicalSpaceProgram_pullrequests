9098ec3f5ce4c93a6c514969e6ed2913fbbe401a
NythicalNorm <cephandriushoid@outlook.com>
2025-12-19 15:49:32 +0530

    Generated planet textures are now sent to the client and rendered as the planet's texture properly.

--- src/main/java/com/nythicalnorm/nythicalSpaceProgram/planetshine/textures/PlanetTexManager.java
+++ src/main/java/com/nythicalnorm/nythicalSpaceProgram/planetshine/textures/PlanetTexManager.java
@@ -6,7 +6,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.DynamicTexture;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.resources.ResourceLocation;
-import org.lwjgl.system.MemoryUtil;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -22,15 +21,19 @@ public class PlanetTexManager {
 
     public void incomingTexture(String planetName, byte[] tex) {
         NythicalSpaceProgram.log(planetName + " texture received, Size: " + tex.length);
-        ByteBuffer bytebuffer = MemoryUtil.memAlloc(tex.length);
+        ByteBuffer texBytebuffer = ByteBuffer.allocateDirect(tex.length);
+        texBytebuffer.put(tex);
+        texBytebuffer.rewind();
 
         try {
-            NativeImage planetImage = NativeImage.read(bytebuffer.put(tex));
+            NativeImage planetImage = NativeImage.read(texBytebuffer);
             DynamicTexture texture = new DynamicTexture(planetImage);
             TextureManager texturemanager = Minecraft.getInstance().getTextureManager();
-            ResourceLocation texResourceLocation = texturemanager.register("nythicalspaceprogram:textures/planets/" + planetName, texture);
+            ResourceLocation texResourceLocation = texturemanager.register("nythicalspaceprogram/planets/" + planetName, texture);
             planetResourceLocations.put(planetName, texResourceLocation);
+
         } catch (IOException e) {
+            NythicalSpaceProgram.logError(e.toString());
             NythicalSpaceProgram.logError("png texture can't be parsed");
         }
     }
